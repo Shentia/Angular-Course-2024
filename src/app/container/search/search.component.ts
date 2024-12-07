67123101def872f08f1fa4adbad1af24b6101934
Ahmadreza Shamimi <ahmadreza.shamimi@gmail.com>
2024-12-07 00:05:17 -0500

    First Commit

---
+++ src/app/container/search/search.component.ts
@@ -0,0 +1,29 @@
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css',
+})
+export class SearchComponent {
+  searchText: string = '';
+  @ViewChild('searchInputText') searchInputEl: ElementRef;
+
+  @Output() searchTextChanged: EventEmitter<string> =
+    new EventEmitter<string>();
+
+  onSearchTextChanged() {}
+
+  updateSearchText() {
+    // console.log(inputEl.value);
+    this.searchText = this.searchInputEl.nativeElement.value;
+    //Notice that we are emitting the searchText value to the parent component.
+    this.searchTextChanged.emit(this.searchText);
+  }
+}
---
+++ src/header/alert-menu/alert-menu.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'alert-menu',
+  templateUrl: './alert-menu.component.html',
+  styleUrl: './alert-menu.component.css',
+})
+export class AlertMenuComponent {}
